ed9972fa575e2a8acdb8b21c182b630cda3c3fdb
Joshua Terriquez <74273335+Joshua-Terriquez@users.noreply.github.com>
2022-10-12 20:09:40 -0700

    Update main.cpp

--- PA2_support_code1/BasicOpenGLProject/main.cpp
+++ PA2_support_code1/BasicOpenGLProject/main.cpp
@@ -2,22 +2,20 @@
 #include <GL/freeglut.h>
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
-
+#include <vector>
 #include <iostream>
 #include "shader.h"
 #include "shaderprogram.h"
-
-#include <math.h>
-#include <vector>
 using namespace std;
+
 /*=================================================================================================
 	DOMAIN
 =================================================================================================*/
 
 // Window dimensions
-const int InitWindowWidth  = 800;
+const int InitWindowWidth = 800;
 const int InitWindowHeight = 800;
-int WindowWidth  = InitWindowWidth;
+int WindowWidth = InitWindowWidth;
 int WindowHeight = InitWindowHeight;
 
 // Last mouse cursor position
@@ -39,9 +37,9 @@ bool draw_wireframe = false;
 ShaderProgram PassthroughShader;
 ShaderProgram PerspectiveShader;
 
-glm::mat4 PerspProjectionMatrix( 1.0f );
-glm::mat4 PerspViewMatrix( 1.0f );
-glm::mat4 PerspModelMatrix( 1.0f );
+glm::mat4 PerspProjectionMatrix(1.0f);
+glm::mat4 PerspViewMatrix(1.0f);
+glm::mat4 PerspModelMatrix(1.0f);
 
 float perspZoom = 1.0f, perspSensitivity = 0.35f;
 float perspRotationX = 0.0f, perspRotationY = 0.0f;
@@ -50,11 +48,79 @@ float perspRotationX = 0.0f, perspRotationY = 0.0f;
 	OBJECTS
 =================================================================================================*/
 
+
 GLuint axis_VAO;
 GLuint axis_VBO[2];
 
-GLuint torus_VAO;   //copied above
-GLuint torus_VBO[2]; //copied above
+// vector<float> vert0 = {
+// 	//x axis
+// 	-1.0f,  0.0f,  1.0f, 1.0f,
+// 	1.0f,  0.0f,  0.0f, 1.0f,
+// 	//y axis
+// 	0.0f, -1.0f,  0.0f, 1.0f,
+// 	0.0f,  1.0f,  0.0f, 1.0f,
+// 	//z axis
+// 	0.0f,  0.0f, -1.0f, 1.0f,
+// 	0.0f,  0.0f,  1.0f, 1.0f
+// };
+
+// auto verts = constructTorus(1.0f, 0.1f, 16, 16);
+
+float R = 1.f;
+float r = 0.2f;
+int N = 32, M = 16;
+
+vector<float> verts;
+// bool updateFlag = true;
+
+struct Point {
+	float x = 0, y = 0, z = 0, w = 1;
+};
+
+
+float PI = 3.1415;
+
+vector<float> constructTorus() { //float R, float r, int N, int M) {
+	std::vector<float> points;
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < M; ++j) {
+
+			auto makePoint = [&](int i, int j) {
+				Point P;
+				float theta = 2 * PI * i / (float)N;
+				float phi = 2 * PI * j / (float)M;
+
+				P.x = (R + r * cos(theta)) * cos(phi);
+				P.y = (R + r * cos(theta)) * sin(phi);
+				P.z = r * sin(theta);
+				return P;
+			};
+
+			Point P = makePoint(i, j);
+			Point Q = makePoint(i, j + 1);
+			Point R = makePoint(i + 1, j + 1);
+			Point S = makePoint(i + 1, j);
+
+			auto add = [&](Point w) {
+				points.push_back(w.x);
+				points.push_back(w.y);
+				points.push_back(w.z);
+				points.push_back(1);
+			};
+
+			/*add(P);  add(Q);
+			add(Q);  add(R);
+			*/
+			add(P);  add(Q);  add(R);
+			add(R);  add(S);  add(P);
+
+
+		}
+	}
+	return points;
+}
+
+
 
 float axis_vertices[] = {
 	//x axis
@@ -68,6 +134,9 @@ float axis_vertices[] = {
 	0.0f,  0.0f,  1.0f, 1.0f
 };
 
+
+
+
 float axis_colors[] = {
 	//x axis
 	1.0f, 0.0f, 0.0f, 1.0f,
@@ -79,181 +148,91 @@ float axis_colors[] = {
 	0.0f, 0.0f, 1.0f, 1.0f,
 	0.0f, 0.0f, 1.0f, 1.0f
 };
-//R is the dist. from the center of the tube to the ce nter of the torus
-   // r is the radius of the tube
-   // R- "major radius" & r - minor radius
-   //angle theta represents rotation around the tube
-  //where phi represents rotation around the torus axis of Revolution
-
-float n =(float)6;
-
-float s = 1.0f;
-float R = 1.0f, r = 0.5f;
-
-vector<float>torus_vertices;
-
-class torus {
-	public:
-
-	float Pi =(float) 3.1416;
-	float twoPi = (float)2 * Pi;
-	float t_x, t_y, t_z=0.0f;
-	float i,j = (float)0;
-
-
-	void torus_param(float R, float r, float n, float s) {
-		
-		for ( i = 0; i < n; i += ((twoPi) / n)) {
-			
-			for (j = 0; j < n; j += ((twoPi) / n))
-			
-				t_x = (R + r * cos(i)) * cos(j);
-				t_y = (R + r * cos(i)) * sin(j);
-				t_z = (r * sin(i));
-
-				torus_vertices.push_back(t_x);
-				torus_vertices.push_back(t_y);
-				torus_vertices.push_back(t_z);
-				//torus_vertices.push_back(s);
-			
-			
-		}
-	}
-
-	//float torus_vertices[]{};
-	    // how do i pass or code array into here? because it requires vertices.
-		// is there another way? instead of using this function?
-	 
-		///float PI = (float)M_PI;
-	/*	float du = 2 * Pi / R;
-	float dv = 2 * Pi / r;
-
-	for (float i = 0; i < R; i++) {
-
-		float u = i * du;
-
-		for (float j = 0; j <= r; j++) {
-
-			float v = (j % r) * dv;
-
-			for (float k = 0; k < 2; k++)
-			{
-				float uu = u + k * du;
-				// compute vertex
-				float t_x = (R + r * cos(v)) * cos(uu);
-				float t_y = (R + r * cos(v)) * sin(uu);
-				float t_z = r * sin(v);
-
-				// add vertex 
-				torus_vertices.push_back(t_x);
-				torus_vertices.push_back(t_y);
-				torus_vertices.push_back(t_z);
-			
-			*/
-}; torus T;
-
-void print() { // this was also made.
-	T.torus_param(R, r, n, s);
-	int count = 0;
-	
-	for (int i = 0; i < torus_vertices.size(); i++) {
-		cout << torus_vertices[i] << endl;
-		count++;
-	}
-	cout << count << endl;
-};
 
 /*=================================================================================================
 	HELPER FUNCTIONS
 =================================================================================================*/
 
-void window_to_scene( int wx, int wy, float& sx, float& sy )
+void window_to_scene(int wx, int wy, float& sx, float& sy)
 {
-	sx = ( 2.0f * (float)wx / WindowWidth ) - 1.0f;
-	sy = 1.0f - ( 2.0f * (float)wy / WindowHeight );
+	sx = (2.0f * (float)wx / WindowWidth) - 1.0f;
+	sy = 1.0f - (2.0f * (float)wy / WindowHeight);
 }
 
 /*=================================================================================================
 	SHADERS
 =================================================================================================*/
 
-void CreateTransformationMatrices( void )
+void CreateTransformationMatrices(void)
 {
 	// PROJECTION MATRIX
-	PerspProjectionMatrix = glm::perspective<float>( glm::radians( 60.0f ), (float)WindowWidth / (float)WindowHeight, 0.01f, 1000.0f );
+	PerspProjectionMatrix = glm::perspective<float>(glm::radians(60.0f), (float)WindowWidth / (float)WindowHeight, 0.01f, 1000.0f);
 
 	// VIEW MATRIX
-	glm::vec3 eye   ( 0.0, 0.0, 2.0 );
-	glm::vec3 center( 0.0, 0.0, 0.0 );
-	glm::vec3 up    ( 0.0, 1.0, 0.0 );
+	glm::vec3 eye(0.0, 0.0, 2.0);
+	glm::vec3 center(0.0, 0.0, 0.0);
+	glm::vec3 up(0.0, 1.0, 0.0);
 
-	PerspViewMatrix = glm::lookAt( eye, center, up );
+	PerspViewMatrix = glm::lookAt(eye, center, up);
 
 	// MODEL MATRIX
-	PerspModelMatrix = glm::mat4( 1.0 );
-	PerspModelMatrix = glm::rotate( PerspModelMatrix, glm::radians( perspRotationX ), glm::vec3( 1.0, 0.0, 0.0 ) );
-	PerspModelMatrix = glm::rotate( PerspModelMatrix, glm::radians( perspRotationY ), glm::vec3( 0.0, 1.0, 0.0 ) );
-	PerspModelMatrix = glm::scale( PerspModelMatrix, glm::vec3( perspZoom ) );
+	PerspModelMatrix = glm::mat4(1.0);
+	PerspModelMatrix = glm::rotate(PerspModelMatrix, glm::radians(perspRotationX), glm::vec3(1.0, 0.0, 0.0));
+	PerspModelMatrix = glm::rotate(PerspModelMatrix, glm::radians(perspRotationY), glm::vec3(0.0, 1.0, 0.0));
+	PerspModelMatrix = glm::scale(PerspModelMatrix, glm::vec3(perspZoom));
 }
 
-void CreateShaders( void )
+void CreateShaders(void)
 {
+	cout << "A" << endl;
 	// Renders without any transformations
-	PassthroughShader.Create( "./shaders/simple.vert", "./shaders/simple.frag" );
+	PassthroughShader.Create("./shaders/simple.vert", "./shaders/simple.frag");
+	cout << "B" << endl;
 
 	// Renders using perspective projection
-	PerspectiveShader.Create( "./shaders/persp.vert", "./shaders/persp.frag" );
+	PerspectiveShader.Create("./shaders/persp.vert", "./shaders/persp.frag");
+	cout << "C" << endl;
 }
 
 /*=================================================================================================
 	BUFFERS
 =================================================================================================*/
 
-void CreateAxisBuffers( void )
+
+void CreateAxisBuffers(void)
 {
-	glGenVertexArrays( 1, &axis_VAO );
-	glBindVertexArray( axis_VAO );
 
-	glGenBuffers( 2, &axis_VBO[0] );
+	verts = constructTorus(); // 1.0f, 0.1f, N, M);
 
-	glBindBuffer( GL_ARRAY_BUFFER, axis_VBO[0] );
-	glBufferData( GL_ARRAY_BUFFER, sizeof( axis_vertices ), axis_vertices, GL_STATIC_DRAW );
-	glVertexAttribPointer( 0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof( float ), (void*)0 );
-	glEnableVertexAttribArray( 0 );
 
-	glBindBuffer( GL_ARRAY_BUFFER, axis_VBO[1] );
-	glBufferData( GL_ARRAY_BUFFER, sizeof( axis_colors ), axis_colors, GL_STATIC_DRAW );
-	glVertexAttribPointer( 1, 4, GL_FLOAT, GL_FALSE, 4 * sizeof( float ), (void*)0 );
-	glEnableVertexAttribArray( 1 );
+	glGenVertexArrays(1, &axis_VAO);
+	glBindVertexArray(axis_VAO);
 
-	glBindVertexArray( 0 );
-}
-void CreateTorusBuffers(void)
-{
-	//print();
-	//print2();
+	glGenBuffers(2, &axis_VBO[0]);
 
-	glGenVertexArrays(1, &torus_VAO);
-	glBindVertexArray(torus_VAO);
-
-	glGenBuffers(2, &torus_VBO[0]);
-
-	glBindBuffer(GL_ARRAY_BUFFER, torus_VBO[0]);
-
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(float)*(24.0F*n*n), &torus_vertices, GL_STATIC_DRAW);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * torus_vertices.size(), &torus_vertices, GL_STATIC_DRAW);
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(&torus_vertices), &torus_vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, axis_VBO[0]);
+	//glBufferData( GL_ARRAY_BUFFER, sizeof( axis_vertices ), axis_vertices, GL_STATIC_DRAW );
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * verts.size(), &verts[0], GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, torus_VBO[1]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(axis_colors), axis_colors, GL_STATIC_DRAW);
+	vector<float> rgb;
+	for (auto v : verts) {
+		rgb.push_back(0.f);
+		rgb.push_back(66.f);
+		rgb.push_back(255.f);
+		rgb.push_back(1.f);
+	}
+
+	glBindBuffer(GL_ARRAY_BUFFER, axis_VBO[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * rgb.size(), &rgb[0], GL_STATIC_DRAW);
 	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(1);
 
 	glBindVertexArray(0);
 }
+
 /*=================================================================================================
 	CALLBACKS
 =================================================================================================*/
@@ -270,56 +249,66 @@ void idle_func()
 	glutPostRedisplay();
 }
 
-void reshape_func( int width, int height )
+void reshape_func(int width, int height)
 {
-	WindowWidth  = width;
+	WindowWidth = width;
 	WindowHeight = height;
 
-	glViewport( 0, 0, width, height );
+	glViewport(0, 0, width, height);
 	glutPostRedisplay();
 }
 
-void keyboard_func( unsigned char key, int x, int y )
+void keyboard_func(unsigned char key, int x, int y)
 {
-	key_states[ key ] = true;
+	key_states[key] = true;
 
-	switch( key )
+	switch (key)
 	{
-		case 'w':
-		{
-			draw_wireframe = !draw_wireframe;
-			if( draw_wireframe == true )
-				std::cout << "Wireframes on.\n";
-			else
-				std::cout << "Wireframes off.\n";
-			break;
-		}
+	case 'w':
+	{
+		draw_wireframe = !draw_wireframe;
+		if (draw_wireframe == true)
+			std::cout << "Wireframes on.\n";
+		else
+			std::cout << "Wireframes off.\n";
+		break;
+	}
+
+	case 'n':
+		++N;
+		CreateAxisBuffers();
+		break;
+
+	case 'm':
+		++N;
+		CreateAxisBuffers();
+		break;
 
 		// Exit on escape key press
-		case '\x1B':
-		{
-			exit( EXIT_SUCCESS );
-			break;
-		}
+	case '\x1B':
+	{
+		exit(EXIT_SUCCESS);
+		break;
+	}
 	}
 }
 
-void key_released( unsigned char key, int x, int y )
+void key_released(unsigned char key, int x, int y)
 {
-	key_states[ key ] = false;
+	key_states[key] = false;
 }
 
-void key_special_pressed( int key, int x, int y )
+void key_special_pressed(int key, int x, int y)
 {
-	key_special_states[ key ] = true;
+	key_special_states[key] = true;
 }
 
-void key_special_released( int key, int x, int y )
+void key_special_released(int key, int x, int y)
 {
-	key_special_states[ key ] = false;
+	key_special_states[key] = false;
 }
 
-void mouse_func( int button, int state, int x, int y )
+void mouse_func(int button, int state, int x, int y)
 {
 	// Key 0: left button
 	// Key 1: middle button
@@ -327,52 +316,52 @@ void mouse_func( int button, int state, int x, int y )
 	// Key 3: scroll up
 	// Key 4: scroll down
 
-	if( x < 0 || x > WindowWidth || y < 0 || y > WindowHeight )
+	if (x < 0 || x > WindowWidth || y < 0 || y > WindowHeight)
 		return;
 
 	float px, py;
-	window_to_scene( x, y, px, py );
+	window_to_scene(x, y, px, py);
 
-	if( button == 3 )
+	if (button == 3)
 	{
 		perspZoom += 0.03f;
 	}
-	else if( button == 4 )
+	else if (button == 4)
 	{
-		if( perspZoom - 0.03f > 0.0f )
+		if (perspZoom - 0.03f > 0.0f)
 			perspZoom -= 0.03f;
 	}
 
-	mouse_states[ button ] = ( state == GLUT_DOWN );
+	mouse_states[button] = (state == GLUT_DOWN);
 
 	LastMousePosX = x;
 	LastMousePosY = y;
 }
 
-void passive_motion_func( int x, int y )
+void passive_motion_func(int x, int y)
 {
-	if( x < 0 || x > WindowWidth || y < 0 || y > WindowHeight )
+	if (x < 0 || x > WindowWidth || y < 0 || y > WindowHeight)
 		return;
 
 	float px, py;
-	window_to_scene( x, y, px, py );
+	window_to_scene(x, y, px, py);
 
 	LastMousePosX = x;
 	LastMousePosY = y;
 }
 
-void active_motion_func( int x, int y )
+void active_motion_func(int x, int y)
 {
-	if( x < 0 || x > WindowWidth || y < 0 || y > WindowHeight )
+	if (x < 0 || x > WindowWidth || y < 0 || y > WindowHeight)
 		return;
 
 	float px, py;
-	window_to_scene( x, y, px, py );
+	window_to_scene(x, y, px, py);
 
-	if( mouse_states[0] == true )
+	if (mouse_states[0] == true)
 	{
-		perspRotationY += ( x - LastMousePosX ) * perspSensitivity;
-		perspRotationX += ( y - LastMousePosY ) * perspSensitivity;
+		perspRotationY += (x - LastMousePosX) * perspSensitivity;
+		perspRotationX += (y - LastMousePosY) * perspSensitivity;
 	}
 
 	LastMousePosX = x;
@@ -383,33 +372,27 @@ void active_motion_func( int x, int y )
 	RENDERING
 =================================================================================================*/
 
-void display_func( void )
+void display_func(void)
 {
-	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	CreateTransformationMatrices();
 
 	PerspectiveShader.Use();
-	PerspectiveShader.SetUniform( "projectionMatrix", glm::value_ptr( PerspProjectionMatrix ), 4, GL_FALSE, 1 );
-	PerspectiveShader.SetUniform( "viewMatrix", glm::value_ptr( PerspViewMatrix ), 4, GL_FALSE, 1 );
-	PerspectiveShader.SetUniform( "modelMatrix", glm::value_ptr( PerspModelMatrix ), 4, GL_FALSE, 1 );
+	PerspectiveShader.SetUniform("projectionMatrix", glm::value_ptr(PerspProjectionMatrix), 4, GL_FALSE, 1);
+	PerspectiveShader.SetUniform("viewMatrix", glm::value_ptr(PerspViewMatrix), 4, GL_FALSE, 1);
+	PerspectiveShader.SetUniform("modelMatrix", glm::value_ptr(PerspModelMatrix), 4, GL_FALSE, 1);
 
-	if( draw_wireframe == true )
-		glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+	if (draw_wireframe == true)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-	glBindVertexArray( axis_VAO );
-	glDrawArrays( GL_LINES, 0, 6 );
+	glBindVertexArray(axis_VAO);
+	glDrawArrays(GL_TRIANGLES, 0, verts.size());
 
-	glBindVertexArray(torus_VAO);// binds the vertex array object with torus_VAO
+	glBindVertexArray(0);
 
-	//Specifies what kind of primitives to render,starting index enabled arrays,
-	// specifies the number of indices to be rendered.
-	glDrawArrays(GL_TRIANGLES, 0, 6);
-
-	glBindVertexArray( 0 );
-
-	if( draw_wireframe == true )
-		glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
+	if (draw_wireframe == true)
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
 	glutSwapBuffers();
 }
@@ -418,25 +401,24 @@ void display_func( void )
 	INIT
 =================================================================================================*/
 
-void init( void )
+void init(void)
 {
 	// Print some info
-	std::cout << "Vendor:         " << glGetString( GL_VENDOR   ) << "\n";
-	std::cout << "Renderer:       " << glGetString( GL_RENDERER ) << "\n";
-	std::cout << "OpenGL Version: " << glGetString( GL_VERSION  ) << "\n";
-	std::cout << "GLSL Version:   " << glGetString( GL_SHADING_LANGUAGE_VERSION ) << "\n\n";
+	std::cout << "Vendor:         " << glGetString(GL_VENDOR) << "\n";
+	std::cout << "Renderer:       " << glGetString(GL_RENDERER) << "\n";
+	std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << "\n";
+	std::cout << "GLSL Version:   " << glGetString(GL_SHADING_LANGUAGE_VERSION) << "\n\n";
 
 	// Set OpenGL settings
-	glClearColor( 0.0f, 0.0f, 0.0f, 0.0f ); // background color
-	glEnable( GL_DEPTH_TEST ); // enable depth test
-	glEnable( GL_CULL_FACE ); // enable back-face culling
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // background color
+	glEnable(GL_DEPTH_TEST); // enable depth test
+	glEnable(GL_CULL_FACE); // enable back-face culling
 
 	// Create shaders
 	CreateShaders();
 
 	// Create buffers
 	CreateAxisBuffers();
-	CreateTorusBuffers();// torus buffers
 
 	std::cout << "Finished initializing...\n\n";
 }
@@ -445,42 +427,36 @@ void init( void )
 	MAIN
 =================================================================================================*/
 
-int main( int argc, char** argv )
+int main(int argc, char** argv)
 {
+	glutInit(&argc, argv);
 
-	
-	print2();
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(InitWindowWidth, InitWindowHeight);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
 
-	glutInit( &argc, argv );
-
-	glutInitWindowPosition( 100, 100 );
-	glutInitWindowSize( InitWindowWidth, InitWindowHeight );
-	glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH );
-
-	glutCreateWindow( "CSE-170 Computer Graphics" );
+	glutCreateWindow("CSE-170 Computer Graphics");
 
 	// Initialize GLEW
 	GLenum ret = glewInit();
-	if( ret != GLEW_OK ) {
+	if (ret != GLEW_OK) {
 		std::cerr << "GLEW initialization error." << std::endl;
-		glewGetErrorString( ret );
+		glewGetErrorString(ret);
 		return -1;
 	}
 
-	glutDisplayFunc( display_func );
-	glutIdleFunc( idle_func );
-	glutReshapeFunc( reshape_func );
-	glutKeyboardFunc( keyboard_func );
-	glutKeyboardUpFunc( key_released );
-	glutSpecialFunc( key_special_pressed );
-	glutSpecialUpFunc( key_special_released );
-	glutMouseFunc( mouse_func );
-	glutMotionFunc( active_motion_func );
-	glutPassiveMotionFunc( passive_motion_func );
+	glutDisplayFunc(display_func);
+	glutIdleFunc(idle_func);
+	glutReshapeFunc(reshape_func);
+	glutKeyboardFunc(keyboard_func);
+	glutKeyboardUpFunc(key_released);
+	glutSpecialFunc(key_special_pressed);
+	glutSpecialUpFunc(key_special_released);
+	glutMouseFunc(mouse_func);
+	glutMotionFunc(active_motion_func);
+	glutPassiveMotionFunc(passive_motion_func);
 
 	init();
-
-	//print();
 
 	glutMainLoop();
 
